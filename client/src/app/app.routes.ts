48c347e01e01c2f8c703d5c6229833fa5650e807
Rakesh <chipparakesh01@gmail.com>
2023-11-27 17:12:45 +0530

    add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TopperComponent } from './topper/topper.component';
+import { AddAnswersComponent } from './add-answers/add-answers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'add-topper', component: TopperComponent },
+    { path: 'add-answers', component: AddAnswersComponent },
+    { path: '', redirectTo: '/add-answers', pathMatch: 'full' }
+];
