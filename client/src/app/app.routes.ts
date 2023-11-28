4785b8dd7e36afe8b3c65cd429e05043031c0aa8
Rakesh <chipparakesh01@gmail.com>
2023-11-28 17:50:23 +0530

    add topic component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { TopperComponent } from './topper/topper.component';
 import { AddTopicsComponent } from './add-topics/add-topics.component';
 import { TopicListComponent } from './topic-list/topic-list.component';
 import { TopperListComponent } from './topper-list/topper-list.component';
+import { TopicComponent } from './topic/topic.component';
 
 export const routes: Routes = [
     { path: 'topicList/:id', component: TopicListComponent },
     { path: 'topperList', component: TopperListComponent },
     { path: 'add-topper', component: TopperComponent },
     { path: 'add-topics', component: AddTopicsComponent },
+    { path: 'topic', component: TopicComponent },
     { path: '', redirectTo: '/topperList', pathMatch: 'full' }
 ];
