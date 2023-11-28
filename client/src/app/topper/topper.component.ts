317bbce5e27931c16b42d3170b9743dc97155f95
Rakesh <chipparakesh01@gmail.com>
2023-11-28 21:49:08 +0530

    refactored the code

--- client/src/app/topper/topper.component.ts
+++ client/src/app/topper/topper.component.ts
@@ -15,40 +15,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './topper.component.html',
   styleUrl: './topper.component.css'
 })
-export class TopperComponent implements OnInit {
-  topperForm!: FormGroup;
-  constructor() { }
+export class TopperComponent {
 
-  ngOnInit(): void {
-
-    this.topperForm = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      rank: new FormControl(null, Validators.required),
-      year: new FormControl(null, Validators.required),
-      gs_1_marks: new FormControl(null, Validators.required),
-      gs_2_marks: new FormControl(null, Validators.required),
-      gs_3_marks: new FormControl(null, Validators.required),
-      gs_4_marks: new FormControl(null, Validators.required),
-      essay_marks: new FormControl(null, Validators.required),
-      prelims_score_gs: new FormControl(null, Validators.required),
-      prelims_score_csat: new FormControl(null, Validators.required),
-      optional_subject: new FormControl(null, Validators.required),
-      optional_1_marks: new FormControl(null, Validators.required),
-      optional_2_marks: new FormControl(null, Validators.required),
-      remarks: new FormControl(null, Validators.required),
-
-    });
-  }
-
-
-
-  submit() {
-    if (this.topperForm.valid) {
-      // this.service
-      //   .addQuestion(this.questionForm.value)
-      //   .subscribe(() => {
-      //     this.questionForm.reset();
-      //   });
-    }
-  }
 }
