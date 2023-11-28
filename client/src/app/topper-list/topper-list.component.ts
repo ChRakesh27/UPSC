4014e7a710afb7d67564935a244dde4b18758f57
Rakesh <chipparakesh01@gmail.com>
2023-11-28 21:49:19 +0530

    added Mat dialog box

--- client/src/app/topper-list/topper-list.component.ts
+++ client/src/app/topper-list/topper-list.component.ts
@@ -2,23 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppService } from '../app.service';
 import { RouterLink } from '@angular/router';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-topper-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatDialogModule],
   templateUrl: './topper-list.component.html',
   styleUrl: './topper-list.component.css'
 })
 export class TopperListComponent implements OnInit {
   title = 'client';
+  model = true
   toppers: any;
-  constructor(private service: AppService) { }
+  constructor(private service: AppService, public dialog: MatDialog) { }
   ngOnInit(): void {
     this.service.getAllToppers().subscribe(list => {
       this.toppers = list
     })
   }
+
+  openDialog() {
+    // const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log(`Dialog result: ${result}`);
+    // });
+  }
+
+
+
+
+
 }
 
 
